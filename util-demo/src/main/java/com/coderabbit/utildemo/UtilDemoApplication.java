b703cca53850dbee25f7993a0169089f8235da88
jiangshaohua <jiangshaohua@memect.co>
2021-12-21 17:08:24 +0800

    spring-actuator

---
+++ util-demo/src/main/java/com/coderabbit/utildemo/UtilDemoApplication.java
@@ -0,0 +1,13 @@
+package com.coderabbit.utildemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UtilDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UtilDemoApplication.class, args);
+    }
+
+}
